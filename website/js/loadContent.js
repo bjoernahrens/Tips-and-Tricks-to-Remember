0b368851d9116d105e3136baddf4b369c7a4aad1
bjoernahrens <ahrens.bjoern@icloud.com>
2020-03-05 22:08:18 +0100

    Updated website and scripts

--- website/js/loadContent.js
+++ website/js/loadContent.js
@@ -1,36 +1,34 @@
-const getHtmlFromFile = async (file) => {
-    return await fetch(file)
-        .then(async (response) => await response.text());
+const createFileElement = (fileContent, folderName) => {
+    let html = `<div class="file">`
+    html += fileContent;
+    html += `</div><br><hr><br>`
+    document.getElementById(folderName).innerHTML += html;
+}
+const createFolderContainer = folderObject => {
+    const html = `<div class="folder" id="${folderObject.name}"><h1>${folderObject.name}</h1></div>`
+    document.getElementById("content").innerHTML += html;
+    folderObject.children.forEach(file => {
+        if (file.type !== '.html') {
+            
+        } else {
+
+            fetch(file.path)
+            .then(response => response.text())
+            .then(fileContent => createFileElement(fileContent, folderObject.name))
+        }
+    });
 }
 
-const createFilesContainer = async (files) => {
-    let result = await files.reduce(async (acc, f) => {
-        let fileHtml = await getHtmlFromFile(f);
-        console.log(fileHtml)
-        let newContent = `<div>${fileHtml}</div><br><hr><br>`
-        return acc + newContent;
-    }, "")
-    return result;
-}
-
-const createFolderContainer = async (folder) => {
-    let name = folder.name;
-    let files = folder.files;
-    let filesContainer = await createFilesContainer(files);
-    return `<div> <div>${name}</div><div>${filesContainer}</div></div>`
-}
-
-const applyFolderStructure = async () => {
-    await fetch('/folderStructureToBe.json')
+const applyFolderStructure = () => {
+    fetch('./folderStructure.json')
         .then(response => response.text())
-        .then(object => JSON.parse(object))
-        .then((data) => {
-            data.root.forEach(async folder => {
-                let content = await createFolderContainer(folder);
-                document.getElementById('content').innerHTML += content
+        .then(responseObject => JSON.parse(responseObject))
+        .then(json => {
+            json.children.forEach(folderObject => {
+                createFolderContainer(folderObject)
             });
-            return content;
         })
+
 }
 
 window.onload = () => applyFolderStructure();
